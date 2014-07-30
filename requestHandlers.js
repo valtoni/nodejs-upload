f96f82f8b293ae9660dc5662d2c0df92dc2166fd
valtoni <valtoni@gmail.com>
2014-07-29 22:46:11 -0300

    Testing blocking start by 10s

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,12 @@
 function start() {
 	console.log("Request handler 'start' was called");
+	
+	function sleep(milliseconds) {
+		var startTime = new Date().getTime()
+		while (new Date().getTime() < startTime + milliseconds);
+	}
+	
+	sleep(10000);
 	return "Hello start";
 }
 
