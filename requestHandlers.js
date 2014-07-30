e531df700dc36c71d4984964d8c9fdfb6a5024d3
valtoni <valtoni@gmail.com>
2014-07-30 00:40:43 -0300

    Receiving TextArea in start and logging

--- requestHandlers.js
+++ requestHandlers.js
@@ -24,7 +24,7 @@ function start(response, postData) {
 function upload(response, postData) {
 	console.log("Request handler 'upload' was called");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: " + postData);
+	response.write("You've sent the text: " + querystring.parse(postData).text);
 	response.end();
 }
 
