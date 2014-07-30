d87a566575151be141f891d004ecf25c24ee3f71
valtoni <valtoni@gmail.com>
2014-07-29 22:39:53 -0300

    Response added. Oh-oh! Non-blocking request handlers will cause fail

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 function route(handle, pathname) {
 	console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] == 'function') {
-		handle[pathname]();
+		return handle[pathname]();
 	}
 	else {
 		console.log("No request handler for " + pathname);
+		return "404 Not Found."
 	}
 }
 exports.route = route;
