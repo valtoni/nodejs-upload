cf4c4bb782b49523049eaa7274c79b180e4ffad3
valtoni <valtoni@gmail.com>
2014-07-30 00:39:32 -0300

    Upload page response

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
 	console.log("Request handler 'start' was called");
 	
 	var body = '<html>' +
@@ -21,10 +21,10 @@ function start(response) {
 	response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+	response.write("You've sent: " + postData);
 	response.end();
 }
 
--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
 	console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] == 'function') {
-		handle[pathname](response);
+		handle[pathname](response, postData);
 	}
 	else {
 		console.log("No request handler for " + pathname);
